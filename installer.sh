5f76cdd0337532db82a8629f540a914925814875
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-06-01 00:24:34 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -36,8 +36,8 @@ if [ -f $MY_TMP_FILE ]; then
 	if [ $MY_RESULT -eq 0 ]; then
         echo "##########################################################"
         echo "#    weatherplugin $version INSTALLED SUCCESSFULLY            #"
-        echo "#             upload BY BIKO - support on                #"
-        echo "#                    Mod by Fairman                      #"	
+	echo "#                    Mod by Fairman                      #"
+        echo "#             upload BY BIKO - support on                #"	
         echo "#  https://www.tunisia-sat.com/forums/threads/3181413/   #"
         echo "##########################################################"
         echo "#           your Device will RESTART Now                 #"
